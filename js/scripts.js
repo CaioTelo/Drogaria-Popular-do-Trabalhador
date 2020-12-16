8f9b6b1b0f3be033300d01ed57b290820169ca9c
CaioTelo <66971492+CaioTelo@users.noreply.github.com>
2020-12-16 15:47:21 -0300

    Versão 0.1.1

--- js/scripts.js
+++ js/scripts.js
@@ -66,4 +66,33 @@
     });
   
   })(jQuery); // End of use strict
+
+  // Contato Whatsapp
+function wpp () {
+  window.open(`https://${screen.width < 800 ? 'api' : 'web'}.whatsapp.com/send?phone=5561999999&text=Terra Plana`)
+}
+function wppproduto1(){
+  window.open(`https://${screen.width < 800 ? 'api' : 'web'}.whatsapp.com/send?phone=5561999999&text=Olá tenho interesse nesse "produto1" em promoção`)
   
+}
+function wppproduto2(){
+  window.open(`https://${screen.width < 800 ? 'api' : 'web'}.whatsapp.com/send?phone=5561999999&text=Olá tenho interesse nesse "produto2" em promoção`)
+  
+}
+function wppproduto3(){
+  window.open(`https://${screen.width < 800 ? 'api' : 'web'}.whatsapp.com/send?phone=5561999999&text=Olá tenho interesse nesse "produto3" em promoção`)
+  
+}
+function wppproduto4(){
+  window.open(`https://${screen.width < 800 ? 'api' : 'web'}.whatsapp.com/send?phone=5561999999&text=Olá tenho interesse nesse "produto4" em promoção`)
+  
+}
+function wppproduto5(){
+  window.open(`https://${screen.width < 800 ? 'api' : 'web'}.whatsapp.com/send?phone=5561999999&text=Olá tenho interesse nesse "produto5" em promoção`)
+  
+}
+function wppproduto6(){
+  window.open(`https://${screen.width < 800 ? 'api' : 'web'}.whatsapp.com/send?phone=5561999999&text=Olá tenho interesse nesse "produto6" em promoção`)
+  
+}
+
